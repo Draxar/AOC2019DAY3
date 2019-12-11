f7dea4e3c5c11fcc1f2b209045037c1337247025
Draxar <kamelotor@gmail.com>
2019-12-11 14:03:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# IDEAS:
+# BRUTE FORCE: greed where i will mark 1 cabel 
+# and tracking with 2 when they are crossing only when closer than open
+import numpy as np
+
+startx = 5000
+starty = 5000
+
+field = np.zeros((10000, 10000))
+print(field)
